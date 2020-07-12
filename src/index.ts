0fb98f50659dc42174b18ee746eab315f7f04af3
d.burmistrov <d.burmistrov@diginavis.com>
2020-07-12 19:51:49 +0300

    switch to telegraf

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,42 @@
-import TelegramBot = require('node-telegram-bot-api');
+import Telegraf = require('telegraf');
 import dotenv = require('dotenv');
 import process = require('process');
-// import Agent from 'socks5-https-client/lib/Agent';
 import { exec } from 'child_process';
 
 dotenv.config();
 const BOT_TOKEN = process.env.BOT_TOKEN;
 if (!BOT_TOKEN) throw new Error('set your token at .env -> BOT_TOKEN');
 
-const bot = new TelegramBot(BOT_TOKEN, {
-    polling: true,
-    request: {
-        url: `https://api.telegram.org/bot${BOT_TOKEN}/getMe`,
-    },
-} as TelegramBot.ConstructorOptions);
+const bot = new (Telegraf as any)(process.env.BOT_TOKEN);
 
 process.stdout.write('telegram bot was launch\n');
 
 const urlRegExp = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/;
-// run vlc player
-bot.onText(urlRegExp, (msg) => {
-    try {
-        if (!msg.text) return;
-        const url = msg.text.match(urlRegExp)?.[0];
-        if (!url) return;
-        exec(`vlc ${url} --fullscreen --preferred-resolution 720 --play-and-exit`);
-        process.stdout.write('running vlc\n');
-    } catch (e) {
-        process.stdout.write(e);
-    }
-});
 
 // stop vlc player
-bot.onText(/^\/stop/, () => {
+bot.command('stop', () => {
     try {
         exec(`killall vlc`);
         process.stdout.write('stopped vlc\n');
     } catch (e) {
-        process.stdout.write(e);
+        process.stdout.write('comand handling error\n');
+        process.stdout.write(JSON.stringify(e));
     }
 });
+
+// run vlc player
+bot.on('message', (ctx: any) => {
+    const message: string | undefined = ctx.message.text;
+    try {
+        if (!message) return;
+        const url = message.match(urlRegExp)?.[0];
+        if (!url) return;
+        exec(`vlc ${url} --fullscreen --preferred-resolution 720 --play-and-exit`);
+        process.stdout.write('running vlc\n');
+    } catch (e) {
+        process.stdout.write('message handling error\n');
+        process.stdout.write(JSON.stringify(e));
+    }
+});
+
+bot.launch();
